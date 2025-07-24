578a6a0df3cced4222a3452d7249e9a9fc0e78cf
stemoutreach <167037256+stemoutreach@users.noreply.github.com>
2025-07-23 19:50:44 -0500

    Update pf_simple_drive_functions.py

--- code/pf_simple_drive_functions.py
+++ code/pf_simple_drive_functions.py
@@ -42,19 +42,8 @@ def StrafeLeft(speed):
     Board.setMotor(2, speed)
     Board.setMotor(3, speed)
     Board.setMotor(4, -speed)
-        
-def Initialize():
-    
-    MotorStop()
-    Board.setPWMServoPulse(1, 1500, 500)
-    Board.setPWMServoPulse(3, 500, 1000)
-    Board.setPWMServoPulse(4, 2500, 1000)
-    Board.setPWMServoPulse(5, 1000, 1000)
-    Board.setPWMServoPulse(6, 1500, 1000)
-    time.sleep(1)
 
 if __name__ == '__main__':
-    Initialize()
     Forward(40)
     time.sleep(1)
     MotorStop()
