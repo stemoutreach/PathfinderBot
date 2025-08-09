16b6b16cd64070396b8ffaa04c56590809ffd9c3
stemoutreach <167037256+stemoutreach@users.noreply.github.com>
2025-08-09 17:31:12 -0500

    Add files via upload

---
+++ code/pf_Test_Motor.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# coding=utf8
+import time
+import Board as Board
+from pf_StartRobot import initialize_robot
+
+def beep(numberofbeeps):
+    for _ in range(numberofbeeps):
+        Board.setBuzzer(1)   # Turn buzzer on
+        time.sleep(0.1)      # Beep duration
+        Board.setBuzzer(0)   # Turn buzzer off
+        time.sleep(0.3)      # Pause between beeps
+
+initialize_robot()
+
+
+beep(1)  # Servo 1
+Board.setMotor(1, 45)
+time.sleep(0.5)
+Board.setMotor(1, 0)
+time.sleep(1)
+        
+beep(2)  # Servo 2
+Board.setMotor(2, 45)
+time.sleep(0.5)
+Board.setMotor(2, 0)
+time.sleep(1)
+
+beep(3)  # Servo 3
+Board.setMotor(3, 45)
+time.sleep(0.5)
+Board.setMotor(3, 0)
+time.sleep(1)
+
+beep(4)  # Servo 4
+Board.setMotor(4, 45)
+time.sleep(0.5)
+Board.setMotor(4, 0)
+time.sleep(1)
